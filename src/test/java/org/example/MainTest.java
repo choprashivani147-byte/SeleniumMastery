cd4dd42a1f1847d79fa3e8b4601a78272104f768
Shivani <choprashivani147@gmail.com>
2026-05-05 11:25:17 -0600

    updated test file

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,12 +1,21 @@
 package org.example;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
 
 import static org.testng.Assert.assertTrue;
 
 public class MainTest {
     @Test
     public void mainTest() {
-       assertTrue(true);
+        // 1. Create an instance of the Main class
+        Main googlePage = new Main();
+
+        // 2. Call the main() method to get the search button
+        WebElement searchButton = googlePage.main();
+
+        // 3. The 'Aha!' Moment: Assert that the button is actually displayed
+        // If isDisplayed() is false, TestNG will fail this test and show it as Red.
+        assertTrue(searchButton.isDisplayed(), "The Google Search button was not visible on the page!");
     }
 }
